8b1caa1256ba41d28986438d113db412221db412
Edward Powell <git@embolalia.com>
2015-01-10 15:41:25 -0500

    Tweak upload script

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 # Build the site, and upload it to the server. Takes one argument, the user to
 # log in as on the server
 
-USER=$1
+USER=${1:-$USER}
 
 jekyll build
-scp -r _site/* $USER@willie.dftba.net:/var/www/willie/
+scp -r build/* $USER@willie.dftba.net:/var/www/willie/
