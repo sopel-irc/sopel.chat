ddc3149b1c44ea0e64ce770fc86f9ec689fa7c1b
dgw <dgw@technobabbl.es>
2019-01-24 08:01:02 -0600

    Exclude/nuke/cleanup build mess better

--- netlify-build.sh
+++ netlify-build.sh
@@ -2,28 +2,28 @@
 
 echo "Starting custom build script..."
 
+echo "Cleaning up leftover Sopel clone"
+rm -rf sopel/
+
 echo "Installing Sphinx"
 pip install sphinx
 
 echo "Building Jekyll site"
 bundler exec jekyll build
 
-if [ -d "sopel/" ]; then
-    cd sopel
-    git checkout master
-    git pull origin master
-    cd docs
-else
-    git clone --depth 1 https://github.com/sopel-irc/sopel.git sopel
-    cd sopel/docs
-fi
+echo "Getting latest Sopel source (for documentation)"
+git clone --depth 1 https://github.com/sopel-irc/sopel.git sopel
 
 echo "Building Sphinx docs"
+cd sopel/docs
 make html
 cd ../../
 
 echo "Moving Sphinx docs to Jekyll output folder"
 mv sopel/docs/build/html _site/docs
+
+echo "Cleaning up Sopel repo"
+rm -rf sopel/
 
 echo "Should be all done!"
 echo "Finished custom build script."
