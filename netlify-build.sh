2c693120c3400891063edca96e4af26eb9a6afc2
dgw <dgw@technobabbl.es>
2019-01-25 14:32:05 -0600

    Make Sopel a submodule to simplify Netlify build

--- netlify-build.sh
+++ netlify-build.sh
@@ -2,28 +2,19 @@
 
 echo "Starting custom build script..."
 
-echo "Cleaning up leftover Sopel clone"
-rm -rf sopel/
-
 echo "Installing Sphinx"
 pip install sphinx
 
 echo "Building Jekyll site"
 bundler exec jekyll build
 
-echo "Getting latest Sopel source (for documentation)"
-git clone --depth 1 https://github.com/sopel-irc/sopel.git sopel
-
 echo "Building Sphinx docs"
-cd sopel/docs
+cd _sopel/docs
 make html
 cd ../../
 
 echo "Moving Sphinx docs to Jekyll output folder"
-mv sopel/docs/build/html _site/docs
-
-echo "Cleaning up Sopel repo"
-rm -rf sopel/
+mv _sopel/docs/build/html _site/docs
 
 echo "Should be all done!"
 echo "Finished custom build script."
