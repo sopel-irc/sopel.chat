3a19cc950fc57561e06476e70a622bf38c9c087f
dgw <dgw@technobabbl.es>
2019-01-27 14:36:21 -0600

    Remove obsolete upload script [skip ci]

--- upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Build the site, and upload it to the server. Takes one argument, the user to
-# log in as on the server
-
-USER=${1:-$USER}
-
-bundler exec jekyll build
-scp -r _site/* $USER@sopel.chat:/var/www/sopel/
