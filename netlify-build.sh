8a7fcfd71775d543777dd75fdce6a9be1e47d4d6
dgw <dgw@technobabbl.es>
2020-02-18 01:26:47 -0600

    build: Run Sphinx before Jekyll to support docs links in link tags
    Allows e.g. `{% link /docs/cli.html %}`
    
    Including Sphinx's `_static` folder is weird in `_config.yml` because
    Jekyll can't understand subfolders in that setting. jekyll/jekyll#1352
    describes the reason it hasn't been changed to support `docs/_static`.

--- netlify-build.sh
+++ netlify-build.sh
@@ -12,9 +12,6 @@ pip3 install ./_sopel
 echo "Generating plugin command/config pages"
 python3 document_sopel_plugins.py --sopel=_sopel
 
-echo "Building Jekyll site"
-jekyll build
-
 echo "Installing Sopel's dev dependencies"
 pip3 install -r ./_sopel/dev-requirements.txt
 
@@ -23,7 +20,10 @@ cd _sopel/docs
 make html
 cd ../../
 
-echo "Moving Sphinx docs to Jekyll output folder"
-mv _sopel/docs/build/html _site/docs
+echo "Moving Sphinx docs to Jekyll site folder for output"
+mv _sopel/docs/build/html/ docs/
+
+echo "Building Jekyll site"
+jekyll build
 
 echo "Finished custom build script!"
