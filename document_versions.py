f4f119f64bc5cef00728d76d4c244f3220f36dcd
dgw <dgw@technobabbl.es>
2020-09-27 09:53:35 -0500

    WIP: Handle unstable/pre-release versions in latest.json
    Relies on a couple small tweaks to sopel itself. Needs peer review.

--- document_versions.py
+++ document_versions.py
@@ -7,7 +7,7 @@ NEWS file, in the "changelogs" collection of the Jekyll site, so the changelog
 entries will always be up-to-date with the Sopel version checked out for
 building the Sphinx docs.
 
-Copyright 2019 dgw, technobabbl.es
+Copyright 2020 dgw, technobabbl.es
 Licensed under the Eiffel Forum License 2.
 
 https://sopel.chat
@@ -15,6 +15,7 @@ https://sopel.chat
 import argparse
 import glob
 import inspect
+import json
 import os
 import re
 
@@ -23,15 +24,22 @@ def main(argv=None):
     this_dir = os.path.dirname(os.path.abspath(__file__))
 
     parser = argparse.ArgumentParser(
-        description="Sopel changelog page generation utility",
+        description="Sopel version documentation utility",
         usage='%(prog)s [options]'
+        )
+    parser.add_argument(
+        '--sopel',
+        dest='sopel_root',
+        nargs='?',
+        help="Specify the location of Sopel's code.",
+        default=os.path.join(this_dir, '_sopel')
         )
     parser.add_argument(
         '--news',
         dest='news_file',
         nargs='?',
         help="Specify the location of the NEWS file to use.",
-        default=os.path.join(os.path.join(this_dir, '_sopel'), 'NEWS')
+        default=None
         )
     parser.add_argument(
         '--clean',
@@ -52,7 +60,23 @@ def main(argv=None):
         os.remove('latest.json')
         return
 
-    news_file = os.path.abspath(os.path.expanduser(args.news_file))
+    sopel_root = os.path.abspath(os.path.expanduser(args.sopel_root))
+    os.sys.path.insert(0, args.sopel_root)
+
+    try:
+        from sopel import (
+            __version__ as current_version,
+            version_info,
+            _version_info,
+        )
+    except ImportError:
+        print("Sopel isn't importable. Cannot continue.")
+        raise
+
+    if args.news_file:
+        news_file = os.path.abspath(os.path.expanduser(args.news_file))
+    else:
+        news_file = os.path.join(sopel_root, 'NEWS')
 
     print("Generating changelog pages using NEWS file: " + news_file)
 
@@ -74,15 +98,28 @@ def main(argv=None):
 
     print("Done! {} versions documented.".format(len(versions.keys())))
 
-    latest = list(versions.keys())[0]
-    with open('latest.json', 'w') as f:
-        f.write("{{\n"  # double braces so Python doesn't get confused
-                "    \"version\": \"{version}\",\n"
-                "    \"unstable\": \"{version}\",\n"
-                "    \"release_notes\": \"https://sopel.chat/changelog/{version}/\"\n"
-                "}}".format(version=latest))
+    info_dict = {}
+    stable = list(versions.keys())[0]
+    info_dict['version'] = stable
+    info_dict['release_notes'] = 'https://sopel.chat/changelog/{}/'.format(stable)
 
-    print("Generated latest.json file pointing to version {}.".format(latest))
+    if version_info.releaselevel == 'final':
+        info_dict['unstable'] = stable
+    else:
+        info_dict['unstable'] = current_version
+        tag_name = 'v' + re.sub(r'\.(a|b|rc)', r'-\1', current_version)
+        info_dict['unstable_notes'] = (
+            'https://github.com/sopel-irc/sopel/releases/tag/{}'.format(tag_name))
+
+    with open('latest.json', 'w') as f:
+        f.write(json.dumps(info_dict, indent=4))
+
+    if stable != info_dict['unstable']:
+        unstable = ', {} (unstable)'.format(info_dict['unstable'])
+    else:
+        unstable = ''
+    print("Generated latest.json file pointing to {} (stable){}."
+          .format(stable, unstable))
 
 if __name__ == '__main__':
     main()
